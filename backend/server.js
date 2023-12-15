9cedb931a4b43214a6285d882c64b9e11afedb78
Charlesovaga <Ovagacharles@gmail.com>
2023-12-15 10:45:42 +0100

    Joy Restaurant

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Api is running");
-});
-
 // Route for all the products
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -40,6 +36,23 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
